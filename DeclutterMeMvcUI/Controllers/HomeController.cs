8c7ba69a661ff89a42ecc51e7ceaa4e81b38de7f
Brian Fontana <brian@codefoxtrot.com>
2022-08-13 16:14:48 -0400

    Add project files.

---
+++ DeclutterMeMvcUI/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DeclutterMeMvcUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeclutterMeMvcUI.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ DeclutterMeMvcUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DeclutterMeMvcUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
