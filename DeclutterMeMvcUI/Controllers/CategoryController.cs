93d625af345b2592600c5e28b9caae897a501b95
Brian Fontana <brian@codefoxtrot.com>
2022-08-14 13:55:32 -0400

    Add category controller and views
    This includes basic notifications via bootstrap alerts

---
+++ DeclutterMeMvcUI/Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿namespace DeclutterMeMvcUI.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly DeclutterMeDbContext _db;
+
+    public CategoryController(DeclutterMeDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        IEnumerable<Category> categories = _db.Categories.AsNoTracking();
+        return View(categories);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category category)
+    {
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "The DisplayOrder cannot match the Name");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View(category);
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category category)
+    {
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "The DisplayOrder cannot match the Name");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View(category);
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(Category category)
+    {
+        Category? c = _db.Categories.FirstOrDefault(c => c.Id == category.Id);
+
+        if (c == null)
+        {
+            return NotFound();
+        }
+
+        _db.Categories.Remove(c);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+
+        return RedirectToAction("Index");
+    }
+}
