c19289d01579d7bab972395aba6dfaa1119738af
Brian Fontana <brian@codefoxtrot.com>
2022-08-17 20:01:44 -0400

    Begin Category page implementation

--- DeclutterMeBlazorUI/GlobalUsings.cs
+++ DeclutterMeBlazorUI/GlobalUsings.cs
@@ -1,2 +1,3 @@
 ﻿global using DataAccessLibrary.Data;
+global using Microsoft.AspNetCore.Components;
 global using Microsoft.EntityFrameworkCore;
---
+++ DeclutterMeBlazorUI/Pages/Category/Category.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace DeclutterMeBlazorUI.Pages.Category;
+
+public partial class Category
+{
+	[Inject] DeclutterMeDbContext db { get; set; }
+
+	private List<DataAccessLibrary.Entities.Category> _categories;
+
+	protected override async Task OnInitializedAsync()
+	{
+        _categories = await db.Categories.AsNoTracking().ToListAsync();
+    }
+}
